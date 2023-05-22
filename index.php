d5fa3c149a6d4de8f1c5fa1ffced7701a16bc7c3
Juan Senen <juansenen@hotmail.com>
2023-05-22 22:09:29 +0200

    Adding bootstrap

--- index.php
+++ index.php
@@ -2,156 +2,20 @@
 include('./view/header.php');
 include('./view/footer.php');
 ?>
-<div class="container-full">
 
-<!-- Nav -->
-<nav class="navbar navbar-inverse navbar-static-top">
-  <div class="container margin-left-nav">
-    <div class="row">
-      <div class="col-md-2">
-        <!-- Navbar brand (Brainiac) y botón colapsable -->
-        <div class="navbar-header">
-          <button class="navbar-toggle" type="button" data-toggle="collapse" data-target="#navbar-main">
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                      </button>
-          <img src="./resources/img/parche-GATI.png" height="100px" width="100px" alt="">
-        </div>
-      </div>
-
-      <div class="col-md-10">
-        <div class="navbar-collapse collapse" id="navbar-main">
-
-          <!-- Navbar botones -->
-          <ul class="nav navbar-nav horizontal mar-left">
-            <li><a href="inicio.html">Inicio</a></li>
-            <li><a href="miembros.html">¿Quiénes somos?</a></li>
-            <li><a href="contacto.html">Contacto</a></li>
-          </ul>
-
-          <!-- Navbar derecho (Registro, Login) -->
-          <ul class="nav navbar-nav navbar-right horizontal">
-
-            <!-- Registro -->
-            <li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Registrarse<span class="caret"></span></a>
-              <ul class="dropdown-menu dropdown-width">
-                <div class="col-lg-12">
-                  <div class="text-center">
-                    <h3><b>Registro</b></h3></div>
-                  <form>
-                    <div class="form-group">
-                      <input type="text" class="form-control" placeholder="Usuario">
-                    </div>
-                    <div class="form-group">
-                      <input type="email" class="form-control" placeholder="Email">
-                    </div>
-                    <div class="form-group">
-                      <input type="password" class="form-control" placeholder="Contraseña">
-                    </div>
-                    <div class="form-group">
-                      <input type="password" class="form-control" placeholder="Confirmar contraseña">
-                    </div>
-
-                    <!-- Botón Registrase -->
-                    <div class="form-group">
-                      <div class="row">
-                        <div class="col-xs-6 col-xs-offset-3">
-                          <input type="submit" class="form-control btn btn-info" value="Registrarse">
-                        </div>
-                      </div>
-                    </div>
-                  </form>
-                </div>
-              </ul>
-            </li>
-
-            <!-- Inicio de sesión -->
-            <li class="dropdown">
-              <a href="#" class="dropdown-toggle" data-toggle="dropdown">Iniciar sesión <span class="caret"></span></a>
-              <ul class="dropdown-menu dropdown-width">
-                <div class="col-lg-12">
-                  <div class="text-center">
-                    <h3><b>Inicio de sesión</b></h3></div>
-                  <form>
-                    <div class="form-group">
-                      <label for="username">Usuario</label>
-                      <input type="text" class="form-control">
-                    </div>
-                    <div class="form-group">
-                      <label for="password">Contraseña</label>
-                      <input type="password" class="form-control">
-                    </div>
-                    <div class="form-group">
-                      <div class="row">
-                        <div class="col-xs-7">
-                          <input type="checkbox">
-                          <label for="remember">Recordarme</label>
-                        </div>
-                        <div class="col-xs-5 pull-right">
-                          <input type="submit" class="form-control btn btn-success" value="Log In">
-                        </div>
-                      </div>
-                    </div>
-
-                    <div class="form-group">
-                      <div class="row">
-                        <div class="col-lg-12">
-                          <div class="text-center">
-                            <a href="#" class="forgot-password">Olvidaste la contraseña?</a>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    <input type="hidden" class="hide" name="token" id="token" value="a465a2791ae0bae853cf4bf485dbe1b6">
-                  </form>
-                </div>
-              </ul>
-            </li>
-          </ul>
-        </div>
-      </div>
-
-
-    </div>
+<form>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
   </div>
-</nav>
-</div>
-
-
-<section class="container-full">
-
-<!-- Main page -->
-<div class="row" id="full-page">
-
-  <!-- Sidebar -->
-  <div class="col-md-2">
-    <div class="profile-sidebar">
-
-      <!-- Sidebar title -->
-      <div class="profile-user">
-        <div class="profile-name">User</div>
-      </div>
-
-      <!-- Sidebar menu -->
-      <nav class="profile-menu">
-        <ul class="nav navbar vertical">
-          <li>
-            <a href="#"><i class="material-icons">computer</i> Equipos</a>
-          </li>
-         
-           <li>
-            <a href="#"><i class="material-icons">phone</i> Telefonía</a>
-          </li>
-        </ul>
-      </nav>
-    </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1">
   </div>
-
-  <!-- Content -->
-  <div class="col-md-10">
-
+  <div class="mb-3 form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Check me out</label>
   </div>
-</div>
-</section>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
--- view/header.php
+++ view/header.php
@@ -2,19 +2,9 @@
 <html lang="en">
 <head>
 	<meta charset="utf-8" />
-	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0">
-
-	<!-- Latest compiled and minified CSS -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-	<!-- jQuery library -->
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
-	<!-- Latest compiled JavaScript -->
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <!-- Google material icons -->
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-
-	<link href="./resources/css/styles.css" type="text/css" rel="stylesheet" />
-	<title>Prometheus Tech Support</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Prometheus Tech Support</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
 </head>
 <body>
   
