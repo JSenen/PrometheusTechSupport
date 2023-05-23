6c9e988efb7b5b3fb44a2853fc6268e47ee4c822
Juan Senen <juansenen@hotmail.com>
2023-05-23 11:30:42 +0200

    Fronted login view

--- index.php
+++ index.php
@@ -7,17 +7,10 @@ include('./view/footer.php');
   </div> 
   <div id="sidebar-nav">   
     <ul>
-      <li class="active"><a href="#"><i class="fa fa-dashboard"></i> Dashboard</a></li>
-      <li><a href="#"><i class="fa fa-desktop"></i> My Website</a></li>
-      <li><a href="#"><i class="fa fa-usd"></i> Ecommerce</a></li>
-      <li><a href="#"><i class="fa fa-pencil-square-o"></i> My Blog</a></li>
-      <li><a href="#"><i class="fa fa-sitemap"></i> SEO Tools</a></li>
-      <li><a href="#"><i class="fa fa-line-chart"></i> Reporting</a></li>
-      <li><a href="#"><i class="fa fa-comments-o"></i>Social Marketing</a></li>
-      <li><a href="#"><i class="fa fa-map-marker"></i> Get Traffic</a></li>     
-      <li><a href="#"><i class="fa fa-users"></i> Employees</a></li>
-      <li><a href="#"><i class="fa fa-calendar-o"></i> Reservations</a></li>
-      <li><a href="#"><i class="fa fa-calendar"></i> Calendar</a></li>
+      <li class="active"></li>
+      <li><a href="#"><i class="fa fa-desktop"></i> Incidencias</a></li>
+      <li><a href="#"><i class="fa fa-desktop"></i> Equipos</a></li>
+      <li><a href="#"><i class="fa fa-usd"></i> Telefonia</a></li>
     </ul>
   </div>
 </section>
