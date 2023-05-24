0eb53cb57a8ac31b82dfab406630469ae3d912f3
Juan Senen <juansenen@hotmail.com>
2023-05-24 21:58:05 +0200

    Update footer and view GATI

--- view/footer.php
+++ view/footer.php
@@ -1,2 +1,15 @@
+<footer class="footer">
+  <div class="mini-footer bg-light">
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="text-left">
+            <p class="text-left">© 2023 <a href="#">JSenen</a>. All rights reserved. Created by <a href="#">GATI BARCELONA</a></p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
 </body>
 </html>
--- view/view_gati.php
+++ view/view_gati.php
@@ -2,14 +2,9 @@
 include('header.php');
 ?>
 
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <img src="../resources/img/parche-GATI.png" width="150" height="150" alt="">
-  
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
+<nav class="navbar-dark bg-dark navbar-vertical show">
+  <ul class="navbar-nav">
+  <img src="../resources/img/parche-GATI.png" width="100" height="100" alt="" class="footer-img">
       <li class="nav-item active">
         <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
       </li>
@@ -19,12 +14,8 @@ include('header.php');
       <li class="nav-item">
         <a class="nav-link" href="#">Pricing</a>
       </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
-      </li>
     </ul>
-  </div>
 </nav>
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+<?php
+include('footer.php');
+?>
