96c77010b8393507f3a0b0068966f832faa57118
Juan Senen <juansenen@hotmail.com>
2023-06-07 16:52:40 +0200

    Fixed addnewuser email

--- model/login_model.php
+++ model/login_model.php
@@ -68,8 +68,6 @@ function addNewUser(){
     $user_pass = htmlspecialchars(($_POST['pass']));
     $user_mail = htmlspecialchars($_POST['email']);
 
-    //TODO Comprobar campos rellenados correctamente y HASH password
-
     $user = new User();
     $user->addNewUser($user_tip, $user_unit, $user_phone,$user_mail ,$user_pass, $dbh);
     
