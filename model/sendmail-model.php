cdf6aee83114640ed174a08d4cc08854112d5b32
Juan Senen <juansenen@hotmail.com>
2023-06-15 10:39:43 +0200

    Added library phpmailer

---
+++ model/sendmail-model.php
@@ -0,0 +1,7 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require './resources/phpmailer/Exception.php';
+require './resources/phpmailer/PHPMailer.php';
+require './resources/phpmailer/SMTP.php';
